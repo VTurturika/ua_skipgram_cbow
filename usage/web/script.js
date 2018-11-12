631f769ef6895ff5341cfbcbafe62a4b0ac80c0c
Victor Turturika <v.turturika@gmail.com>
2018-11-12 03:37:05 +0200

    Simple frontned with bootstrap and vue

---
+++ usage/web/script.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    word: '',
+    cbow: ['-'],
+    skipgram: ['-']
+  },
+  methods: {
+  	search() {
+  		fetch('http://localhost:5000/get-simular', {
+  			method: 'POST',
+  			headers: {
+  				'Accept': 'application/json',
+      			'Content-Type': 'application/json'
+  			},
+  			body: JSON.stringify({word: this.word})
+  		})
+  		.then(res => res.json())
+  		.then(res => {
+  			if (res.cbow && res.skipgram) {
+  				this.cbow = res.cbow
+  				this.skipgram = res.skipgram	
+  			} else {
+  				this.cbow = ['Невідоме слово']
+  				this.skipgram = ['Невідоме слово']
+  			}
+
+  			this.word = res.word
+  		})
+  	}
+  }
+})
